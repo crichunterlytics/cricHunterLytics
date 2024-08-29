1fe9bef3a09b7eae3ec17a103c9093d629a5b65f
suhaslondhe <suhas.londhe@jetsynthesys.com>
2024-08-29 23:35:43 +0530

    - Added Project structure.

--- CHL-Android/app/src/main/java/com/crickhunterlytics/MainActivity.kt
+++ CHL-Android/app/src/main/java/com/crickhunterlytics/MainActivity.kt
@@ -2,20 +2,50 @@ package com.crickhunterlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.crickhunterlytics.apiCallUtility.ApiClient
+import com.crickhunterlytics.databinding.ActivityMainBinding
+import com.crickhunterlytics.fragement.SeriesList
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        getSeriesList()
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(SeriesList())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(SeriesList())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SeriesList())
+                    true
+                }
+
+                else -> {
+                    loadFragment(SeriesList())
+                    true
+                }
+            }
+        }
+       // getSeriesList()
     }
-
-    private fun getSeriesList() {
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+    /*private fun getSeriesList() {
         val call = ApiClient.apiService.getPostById(postId)
 
         call.enqueue(object : Callback<Post> {
@@ -32,5 +62,5 @@ class MainActivity : AppCompatActivity() {
                 // Handle failure
             }
         })
-    }
+    }*/
 }
