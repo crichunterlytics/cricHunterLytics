453bce72d7a1745b66e550c28996e9319688679f
rupalinile <rupali.nile1406@gmail.com>
2024-09-03 19:28:06 +0530

    Updated changes

--- CHL-Backend/lib/chlDb.js
+++ CHL-Backend/lib/chlDb.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host:'184.168.109.23',
-    user: 'crichunter',
+    user: 'rupalinile',
     database: 'crichunterlytics_staging_db',
-    password: 'Cric@Hunter0824'
+    password: 'Nile@Rupali2003'
 });
 // connection.connect();
 
