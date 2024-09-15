d236baaaae024189d92118fbbff82adec46329ba
rupalinile <rupali.nile1406@gmail.com>
2024-09-15 11:20:10 +0530

    updated changes

--- pass-events-backend/lib/db.js
+++ pass-events-backend/lib/db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host:'184.168.109.23',
+    host: '184.168.109.23',
     user: 'rupalinile',
     database: 'pss_event_management',
-    password: 'Nile@Rupali2003'
+    password: 'Nile@Rupali2003',
+    connectTimeout: 10000, // 10 seconds
+    acquireTimeout: 10000, // 10 seconds
+    keepAlive: true,       // Enable keep-alive
+    connectionLimit: 10    // Limit number of connections to the DB
 });
-// connection.connect();
 
 connection.connect((err) => {
     if (err) {
@@ -16,7 +19,14 @@ connection.connect((err) => {
 });
 
 connection.on('error', (err) => {
-    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+    } else if (err.code === 'ECONNRESET') {
+        console.error('Connection was reset.');
+        // You can also try to reconnect here.
+    } else {
+        console.error('Database error:', err);
+    }
 });
 
 module.exports = connection;
