52b5f2dbff320ebd985770ff2467d0f4b0a6e0bc
rupalinile <rupali.nile1406@gmail.com>
2024-10-01 20:36:10 +0530

    Added changes

--- pass-events-backend/middleware/token_interpreter.js
+++ pass-events-backend/middleware/token_interpreter.js
@@ -5,6 +5,7 @@ const { JWT_SECRET_KEY, TOKEN_UNAUTHORIZED_CODE, TOKEN_INVALID_CODE } = require(
 module.exports = {
     verifyToken : (req, res, next) => {
         console.log("req=", req);
+        console.log(req.headers['user-agent']);
         return next();
         //Skip token verification for login and logout routes
         if (req.path === '/api/login' || 
